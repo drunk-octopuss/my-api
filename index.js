6da01aee7a3ab1c50bd84ab64060d7e4bb44c2dd
Linn <linnvailt@gmail.com>
2023-02-03 12:37:29 +0100

    my api

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+import usersRoutes from './routes/users.js'
+
+const app = express()
+const PORT = 4000
+
+app.use(bodyParser.json())
+
+app.use('/users', usersRoutes)
+
+app.get('/', (req, res) => res.send('Hello, its homepage'))
+
+//Connect to my own DB
+mongoose.connect(
+    `mmongodb+srv://myassuser:Wholetthedogsout@cluster0.p4u5tqu.mongodb.net/Cluster0?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true},
+    () => {
+        console.log('DB connected bitch')
+    }
+)
+
+app.listen(PORT, () => console.log(`server running on port: http://localhost:${PORT}`))
